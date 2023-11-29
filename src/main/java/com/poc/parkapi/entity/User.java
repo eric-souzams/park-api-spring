34cbc43458d35b48e6ada2b10637746c1c6ca4f6
Éric Magalhães <15831837+eric-souzams@users.noreply.github.com>
2023-11-29 20:18:15 -0300

    Implemented Spring JPA Auditing on User Entity

--- src/main/java/com/poc/parkapi/entity/User.java
+++ src/main/java/com/poc/parkapi/entity/User.java
@@ -5,6 +5,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -13,6 +18,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 
     @Id
@@ -30,15 +36,19 @@ public class User implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENT;
 
+    @CreatedDate
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 
+    @LastModifiedDate
     @Column(name = "modified_date")
     private LocalDateTime modifiedDate;
 
+    @CreatedBy
     @Column(name = "created_by")
     private String createdBy;
 
+    @LastModifiedBy
     @Column(name = "modified_by")
     private String modifiedBy;
 
